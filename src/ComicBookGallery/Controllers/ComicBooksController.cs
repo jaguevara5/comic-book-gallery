545de5e9bbb66d42fd75e6025d93501d681b1f44
alanguevara10 <aguevara@ctuit.com>
2018-05-11 16:06:13 -0500

    Added design files

--- src/ComicBookGallery/Controllers/ComicBooksController.cs
+++ src/ComicBookGallery/Controllers/ComicBooksController.cs
@@ -10,7 +10,18 @@ namespace ComicBookGallery.Controllers
 	{
 		public ActionResult Detail()
 		{
-            return View();
+			ViewBag.SeriesTitle = "The Amazing Spider-Man";
+			ViewBag.issueNumber = 700;
+			ViewBag.description = "<p>Final issue! Witness the final hours of Doctor Octopus' life and his one, last, great act of revenge! Even if Spider-Man survives... <strong>will Peter Parker?</strong></p>";
+			ViewBag.artists = new String[]
+			{
+				"Script: Dan Slott",
+				"Pencils: Humberto Ramos",
+				"Inks: Victor Olazaba",
+				"Colors: Edgar Delgado",
+				"Letters: Chris Eliopoulos"
+			};
+			return View();
 		}
 	}
 }
